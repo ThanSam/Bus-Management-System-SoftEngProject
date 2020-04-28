6c86541441b5e9cd3520c731452fdf0bd13c68ac
Thanasis Samaras <62352408+ThanSam@users.noreply.github.com>
2020-04-29 01:51:34 +0300

    Improve the GUI.

--- src/ScheduleGUI.java
+++ src/ScheduleGUI.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,24 +21,31 @@ import javax.swing.border.TitledBorder;
 public class ScheduleGUI extends JFrame{
 	
 	private JPanel panel,printSpecificDriverProgramPanel;
+	private ImageIcon icon;
 	private JTextArea printSpecificDriverProgramTextArea;
+	private JButton BackButton;
+	
 	private Secretariat sec = new Secretariat();
 	private int id;
 	
 	public ScheduleGUI(Secretariat aSecretariat ,Driver aDriver) {
 		this.id=id;
 		sec = aSecretariat;
+		
 		panel = new JPanel();
+		icon = new ImageIcon("p2.png");
 		printSpecificDriverProgramPanel= new JPanel();
-	
+		BackButton = new JButton("Back");
+		
+		
 		int id = Integer.parseInt(aDriver.getId());
-		printSpecificDriverProgramTextArea = new JTextArea(10, 30);
+		printSpecificDriverProgramTextArea = new JTextArea(15, 35);
 		
 		ArrayList<String> scheduleList  = new ArrayList<String>();
 		scheduleList = sec.getSpecificDriverProgramm(id);
 		for (int i= 0; i<scheduleList.size(); i++) {
 			if (scheduleList.get(i)!= null) {
-				printSpecificDriverProgramTextArea.append("\n"+scheduleList.get(i));
+				printSpecificDriverProgramTextArea.append(scheduleList.get(i) + "\nBus Line: 2K\nTime: 14.00, 15.00, 16.00\n\n");
 			}
 		}
 	
@@ -46,15 +54,17 @@ public class ScheduleGUI extends JFrame{
 		JScrollPane scroll1 = new JScrollPane(printSpecificDriverProgramTextArea);
 		scroll1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		printSpecificDriverProgramPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.LIGHT_GRAY));
-		printSpecificDriverProgramPanel.setBorder(new TitledBorder(new EtchedBorder(), "Print Specific Driver Program Area"));
+		printSpecificDriverProgramPanel.setBorder(new TitledBorder(new EtchedBorder(), "My Schedule"));
 		printSpecificDriverProgramPanel.add(scroll1);
 		
 		panel.add(printSpecificDriverProgramPanel);
+		panel.add(BackButton);
 
 		this.setContentPane(panel);
+		this.setIconImage(icon.getImage());
 		this.setVisible(true);
-		this.setSize(700, 300);
-		this.setTitle("ебдоладиаио пяоцяалла");
+		this.setSize(450, 360);
+		this.setTitle("Weekly Schedule");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 	
