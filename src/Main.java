8edbf64107ee29c4d0e7f0c3367965893e9ab77c
Thanasis Samaras <62352408+ThanSam@users.noreply.github.com>
2020-05-03 21:46:03 +0300

    Initiate bus lines and routes.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		
         Secretariat s1 = new Secretariat();
         
-        //Dummy data
+        //Dummy data(Drivers & Passengers)
         Driver d1 = new Driver("John", "Lampis", "22", "6981144627" , "1", "mypass", s1);
 		Driver d2 = new Driver("Kostas", "Kampos", "40", "6941144625" , "2", "mypass2", s1);
 		Driver d3 = new Driver("George", "Dimitriou", "34", "6981145625" , "3", "mypass3", s1);
@@ -18,6 +18,12 @@ public class Main {
 		s1.addDriver(d2);
 		s1.addDriver(d3);
 		s1.addDriver(d4);
+
+		//Initiate bus lines.
+		ArrayList<BusLine> BusLines = new ArrayList<BusLine>(BusLine.initiateBusLines());
+		
+		//Initiate bus routes.
+		ArrayList<BusRoute> BusRoutes = new ArrayList<BusRoute>(BusRoute.initiateBusRoutes());
 		
 		MainFrame mainFrame = new MainFrame(s1);
 		//new DriverGUI(d4,s1);
