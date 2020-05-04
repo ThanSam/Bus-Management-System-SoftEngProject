d7fe185d02f9038f92a0228b1f58e231fa758fd4
Christos68 <62355295+Christos68@users.noreply.github.com>
2020-05-04 19:07:30 +0300

    change this method . now you can use it

--- src/ShowEmegencyFrame.java
+++ src/ShowEmegencyFrame.java
@@ -1,46 +1,53 @@
+import java.awt.Checkbox;
 import java.awt.CheckboxGroup;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
 
+public class ShowEmegencyFrame extends JFrame {
 
-public class ShowEmegencyFrame extends JFrame{
-	
 	private ImageIcon icon;
+	private ImageIcon icon3;
 	private JLabel img;
-	
-	private JTextArea printArea;
-	private JTextArea printArea1;
 	private ArrayList<Driver> emergencyList = new ArrayList<Driver>();
 	private ArrayList<Driver> freeDrivers = new ArrayList<Driver>();
+	private ArrayList<Driver> driverList = new ArrayList<Driver>();
 	private JLabel EmergenciesLabel;
 	private JLabel freeDriversLabel;
-    private JPanel driversWhoPushButtonPanel;
-    private JPanel driversFreePanel;
-    private JButton sendDriverButton;
-    private JButton BackButton;
-    private JPanel mainPanel;
-    private Secretariat secretariat;
+	private JPanel driversWhoPushButtonPanel;
+	private JPanel driversFreePanel;
+	private JButton sendDriverButton;
+	private JButton BackButton;
+	private JButton refreshButton;
+	private JPanel mainPanel;
+	private Secretariat sec;
+	private CheckboxGroup freeDriversBox = new CheckboxGroup();
+	private CheckboxGroup emergencyDriversBox = new CheckboxGroup();
+	private String[][] driverline = new String[10][7];
+	private Checkbox checkBox1;
+	private Checkbox checkBox2;
+    private JScrollPane scroll2;
+    private JScrollPane scroll;
     
-	public ShowEmegencyFrame(Secretariat secretariat) {
-		
+    
+	public ShowEmegencyFrame(Secretariat sec) {
+        this.sec=sec;
 		icon = new ImageIcon("p2.png");
-		img = new JLabel(new ImageIcon("p8.png"));	
-		this.secretariat=secretariat;
-		
+		icon3 = new ImageIcon("p9.png");
+		img = new JLabel(new ImageIcon("p8.png"));
+		refreshButton= new JButton("OK", new ImageIcon("p9.png"));
 		EmergenciesLabel = new JLabel("Emergencies");
 		freeDriversLabel = new JLabel("Free Drivers");
 		driversWhoPushButtonPanel = new JPanel();
@@ -50,86 +57,103 @@ public class ShowEmegencyFrame extends JFrame{
 		sendDriverButton.setForeground(Color.red);
 		mainPanel = new JPanel();
 
-		
-		Driver d1 = new Driver("John", "Lampis", "22", "6981144627" , "JohnL", "mypass", secretariat);
-		Driver d2 = new Driver("Kostas", "Kampos", "40", "6941144625" , "KostasKam", "mypass2", secretariat);
-		Driver d3 = new Driver("George", "Dimitriou", "34", "6981145625" , "GeorgeDim", "mypass3", secretariat);
-		Driver d4 = new Driver("Nick", "Delis", "25", "6973444625" , "NickDel", "mypass6", secretariat);
-		Driver d5 = new Driver("Jim", "Papadopoulos", "27", "6981156789" , "JimPap", "mypass5", secretariat);
-		Driver d6 = new Driver("Antonios", "Konstantinidis", "27", "6981156789" , "AntonisKon", "mypass5", secretariat);
-		
-		freeDrivers.add(d1);
-		freeDrivers.add(d3);
-		freeDrivers.add(d4);
-		freeDrivers.add(d5);
-	
-		
-		//ArrayList<JCheckBox> CheckBoxesD = new ArrayList<JCheckBox>(); 
-		ArrayList<JCheckBox> CheckBoxesFD = new ArrayList<JCheckBox>();
-		
-	
-		JCheckBox D = new JCheckBox("Route: 2K 11:40 Stop:Aristotelous Square"+"  Driver: " + d2.getId());
-		JCheckBox D2 = new JCheckBox("Route: 14 11:55 Stop:Kamara"+"  Driver: " + d6.getId());
-		
 		driversWhoPushButtonPanel.setLayout(new BoxLayout(driversWhoPushButtonPanel, BoxLayout.Y_AXIS));
-		
-		driversWhoPushButtonPanel.add(D);
-		driversWhoPushButtonPanel.add(D2);
-		
-		
-		
-		
-		for(Driver fd:freeDrivers) {
-			
-			JCheckBox freeD = new JCheckBox(fd.getFirstname()+" "+ fd.getLastname());
-			CheckBoxesFD.add(freeD);
-		}
-		
-		driversFreePanel.setLayout(new BoxLayout(driversFreePanel, BoxLayout.Y_AXIS));
-		
-		for(JCheckBox freeD: CheckBoxesFD) {
-			
-			driversFreePanel.add(freeD);
-		}
-		
 
-		
+		emergencyList = sec.getEmergencyList();
+		for (Driver eD : emergencyList) {
+			checkBox2 = new Checkbox(eD.getId() + " " + eD.getFirstname() + " " + eD.getLastname() + " "
+					+ eD.getPhoneNumber() + " " + eD.getLineid(), emergencyDriversBox, false);
+			driversWhoPushButtonPanel.add(checkBox2);
+			
+		}
+
+		freeDrivers = sec.getFreeDriversList();
+		for (Driver fD : freeDrivers) {
+			checkBox1 = new Checkbox(
+					fD.getId() + " " + fD.getFirstname() + " " + fD.getLastname() + " " + fD.getPhoneNumber(),
+					freeDriversBox, false);
+			driversFreePanel.add(checkBox1);
+
+		}
+
+		driversFreePanel.setLayout(new BoxLayout(driversFreePanel, BoxLayout.Y_AXIS));
+
 		JScrollPane scroll = new JScrollPane(driversWhoPushButtonPanel);
 		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scroll.setSize(30, 100);
-		
-		JScrollPane scroll2 = new JScrollPane(driversFreePanel);
+
+		 scroll2 = new JScrollPane(driversFreePanel);
 		scroll2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scroll2.setSize(30, 100);
-		
+
+		ButtonListener b1 = new ButtonListener();
+		sendDriverButton.addActionListener(b1);
+		refreshButton.addActionListener(b1);
+
 		mainPanel.setLayout(null);
-		img.setBounds(180,0,220,200);
+		img.setBounds(180, 0, 375, 200);
 		EmergenciesLabel.setBounds(150, 225, 100, 25);
 		freeDriversLabel.setBounds(460, 225, 100, 25);
-		scroll.setBounds(0, 250, 400, 140);
-		scroll2.setBounds(405, 250, 165, 100);
+		scroll.setBounds(15, 250, 400, 140);
+		scroll2.setBounds(425, 250, 300, 140);
 		sendDriverButton.setBounds(220, 420, 150, 25);
+		refreshButton.setRolloverIcon(new ImageIcon("10.png"));
+		refreshButton.setBounds(475, 400, 25, 25);
 		BackButton.setBounds(475, 430, 80, 25);
-		
+
 		mainPanel.add(img);
 		mainPanel.add(EmergenciesLabel);
 		mainPanel.add(freeDriversLabel);
 		mainPanel.add(scroll);
 		mainPanel.add(scroll2);
 		mainPanel.add(sendDriverButton);
+		mainPanel.add(refreshButton);
 		mainPanel.add(BackButton);
-		
-	
 
 		this.setContentPane(mainPanel);
 		this.setIconImage(icon.getImage());
 		this.setResizable(false);
 		this.setVisible(true);
-		this.setSize(590, 500);
+		this.setSize(750, 500);
 		this.setTitle("Emergencies");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		}
-		
 
+	}
+
+	class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			
+			if (e.getSource().equals(sendDriverButton)) {
+			if(emergencyList.isEmpty()||freeDrivers.isEmpty()) {
+				JOptionPane.showMessageDialog(mainPanel, "You can not matche the drivers");
+
+			}
+			else {
+				 String[] buttons = { "Yes","No"};
+
+				    int rc = JOptionPane.showOptionDialog(null, "Are you sure","",
+				        JOptionPane.WARNING_MESSAGE, 1, null, buttons, buttons[1]);
+
+				    if(rc == 0) {
+				    	String[] freeparts = freeDriversBox.getSelectedCheckbox().getLabel().split(" ");
+				    	String[] emergencyparts = emergencyDriversBox.getSelectedCheckbox().getLabel().split(" ");
+				    	
+				    	driverList=sec.getDriverList();
+				    	for(Driver driver:driverList) {
+				    		if(driver.getId().equals(emergencyparts[0])) {
+				    			emergencyList.remove(driver);}
+				    		if(driver.getId().equals(freeparts[0])) {
+				    			freeDrivers.remove(driver);
+				    		}
+				    		  
+				    	}
+				    }
+			}
+			}
+			else if (e.getSource().equals(refreshButton)) {
+	
+			}
+		}
+
+	}
 }
